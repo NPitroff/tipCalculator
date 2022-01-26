5bcb0a3762e4f44df375fdc1cc7a87e66fc8d35e
NPitroff <nicolai.pitroff@gmail.com>
2022-01-25 19:11:06 -0800

    Added JS to select the values in the calculator

---
+++ index.js
@@ -0,0 +1,9 @@
+function totalTip() {
+  // selecting the check total
+  var checkTotal = document.getElementById("checkTotal").value;
+  // selecting the tip percentage
+  var tipTotal = document.getELementById("tipTotal").value;
+
+  alert(checkTotal);
+  alert(tipTotal);
+}
