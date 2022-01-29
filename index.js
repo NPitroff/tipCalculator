524bfe17b9c07a9a08ea3e320fb6ba72f26edb1c
NPitroff <nicolai.pitroff@gmail.com>
2022-01-28 16:23:54 -0800

    Fixed the calculations to return a number

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function totalTip() {
   // selecting the check total
   var checkTotal = document.getElementById("checkTotal").value;
   // selecting the tip percentage
-  var tipTotal = document.getElementById("tipTotal").value;
-
-  var finalTotal = ((tipTotal/100)*checkTotal) ;
-
-  document.getElementById("finalTotal").innerHTML = finalTotal;
+  var tipTotal = document.getElementById("tipTotal").value/100;
+  console.log("tip total is " + tipTotal)
+   var finalTotal = Number(checkTotal) + Number(tipTotal *checkTotal);
+   console.log("tip amount is " + finalTotal.toFixed(2));
+  //document.getElementById("finalTotal").innerHTML = finalTotal;
 
 }
