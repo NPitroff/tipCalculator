232b4ffaf82141f35dfb4ca33b66c452a89739e3
NPitroff <nicolai.pitroff@gmail.com>
2022-01-28 16:32:07 -0800

    Added js to display the amount to tip

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ function totalTip() {
   //variable to save the Total
    var finalTotal = Number(checkTotal) + Number(tipTotal *checkTotal);
    // console.log("tip amount is " + finalTotal.toFixed(2));
+   // js to display the tip cash Amount
+   document.getElementById("tipAmount").innerHTML = Number(tipTotal * checkTotal).toFixed(2);
+   //js to display the total check with tip
    document.getElementById("finalTotal").innerHTML = finalTotal.toFixed(2);
 
 }
