630e6b7da0ab5c947bdc062d6e17ada8fc45acac
NPitroff <nicolai.pitroff@gmail.com>
2022-01-29 08:34:50 -0800

    Linked a stylesheet and added a card-img-top

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ function totalTip() {
   // selecting the tip percentage
   var tipTotal = document.getElementById("tipTotal").value/100;
   //console.log("tip total is " + tipTotal)
+  
   //variable to save the Total
    var finalTotal = Number(checkTotal) + Number(tipTotal *checkTotal);
    // console.log("tip amount is " + finalTotal.toFixed(2));
+
    // js to display the tip cash Amount
    document.getElementById("tipAmount").innerHTML = Number(tipTotal * checkTotal).toFixed(2);
    //js to display the total check with tip
