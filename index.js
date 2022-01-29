644c8375d0f0a99db947aca093a8e9c6a14ff7b6
NPitroff <nicolai.pitroff@gmail.com>
2022-01-29 08:50:54 -0800

    Moved the tipTotal and checkTotal to global variables

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+// selecting the check total
+var checkTotal = document.getElementById("checkTotal").value;
+// selecting the tip percentage
+var tipTotal = document.getElementById("tipTotal").value/100;
+//console.log("tip total is " + tipTotal)
+
 function totalTip() {
-  // selecting the check total
-  var checkTotal = document.getElementById("checkTotal").value;
-  // selecting the tip percentage
-  var tipTotal = document.getElementById("tipTotal").value/100;
-  //console.log("tip total is " + tipTotal)
-  
+
+
   //variable to save the Total
    var finalTotal = Number(checkTotal) + Number(tipTotal *checkTotal);
    // console.log("tip amount is " + finalTotal.toFixed(2));
@@ -14,4 +16,11 @@ function totalTip() {
    //js to display the total check with tip
    document.getElementById("finalTotal").innerHTML = finalTotal.toFixed(2);
 
+}
+
+// loop to change the card image
+var coinImage = document.getElementById("coins");
+
+if(tipTotal <= .05){
+  coineImage.setAttribute("src", "css/coins1.jpg");
 }
