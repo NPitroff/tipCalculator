4598fab8cec141d5481916e3dd3ac61c33e95b4b
NPitroff <nicolai.pitroff@gmail.com>
2022-02-09 17:15:09 -0800

    refactored js to accept jquery

--- index.js
+++ index.js
@@ -5,17 +5,19 @@ var coinImage = document.getElementById("coins");
 // console.log("Tip value is " + tipTotal);
 function totalTip() {
   // selecting the check total
-  var checkTotal = document.getElementById("checkTotal").value;
+  var checkTotal = $("#checkTotal").val();
+  console.log(checkTotal)
   // selecting the tip percentage
-  var tipTotal = document.getElementById("tipTotal").value/100;
+  var tipTotal = $("#tipTotal").val()/100;
+  console.log(tipTotal)
   //variable to save the Total
    var finalTotal = Number(checkTotal) + Number(tipTotal *checkTotal);
    // console.log("tip amount is " + finalTotal.toFixed(2));
 
    // js to display the tip cash Amount
-   document.getElementById("tipAmount").innerHTML = Number(tipTotal * checkTotal).toFixed(2);
+   $("#tipAmount").innerHTML = Number(tipTotal * checkTotal).toFixed(2);
    //js to display the total check with tip
-   document.getElementById("finalTotal").innerHTML = finalTotal.toFixed(2);
+   $("#finalTotal").innerHTML = finalTotal.toFixed(2);
    // function call to change the coin image
    changeCoin();
 }
